bf595a1bbd9297012d267c1f904bae2f286eeb3d
juneiker <juneikerc@gmail.com>
2025-08-29 15:26:03 -0400

    feat:

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [cloudflare(), tailwindcss(), reactRouter(), tsconfigPaths()],
+  ssr: {
+    target: "webworker",
+  },
 });
